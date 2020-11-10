655a1f47646e35efe490467f30db0639e071b541
Kratos1700 <hunterstarsa@hotmail.com>
2020-11-10 17:47:04 +0100

    Mapeo de datos JSON a objeto

--- app/src/main/java/com/example/solicitudeshttp/JSON/ActivityJSON.kt
+++ app/src/main/java/com/example/solicitudeshttp/JSON/ActivityJSON.kt
@@ -2,11 +2,42 @@ package com.example.solicitudeshttp.JSON
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.solicitudeshttp.R
+import org.json.JSONObject
 
 class ActivityJSON : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_j_s_o_n)
+
+        var respuesta = "{ \"personas\" : [ " +
+                "{" +
+                " \"nombre\" : \"Marcos\" ," +
+                " \"pais\" : \"México\" ," +
+                " \"estado\" : \"soltero\" ," +
+                " \"experiencia\" : 5}," +
+
+                "{" +
+                " \"nombre\" : \"Agustín\" ," +
+                " \"pais\" : \"España\" ," +
+                " \"estado\" : \"casado\" ," +
+                " \"experiencia\" : 16}" +
+                " ]" +
+                " }"
+
+        // obtenim estructura
+        val json = JSONObject(respuesta)
+
+        val personas = json.getJSONArray("personas")
+        for (i in 0..personas.length()-1){
+            val nombre = personas.getJSONObject(i).getString("nombre")
+            val pais = personas.getJSONObject(i).getString("pais")
+            val estado = personas.getJSONObject(i).getString("estado")
+            val experiencia = personas.getJSONObject(i).getInt("experiencia")
+            //mapeix de valors a un objecte
+            val persona = Persona(nombre, pais, estado, experiencia)
+             Log.d("PERSONA", persona.nombre)
+        }
     }
 }
