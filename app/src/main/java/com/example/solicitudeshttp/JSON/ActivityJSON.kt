b40359cae5bdb127746d7f575b09099ec083e457
Kratos1700 <hunterstarsa@hotmail.com>
2020-11-10 19:36:08 +0100

    Mapeo de datos JSON a objeto forma nativa añadida una lista para guardar datos

--- app/src/main/java/com/example/solicitudeshttp/JSON/ActivityJSON.kt
+++ app/src/main/java/com/example/solicitudeshttp/JSON/ActivityJSON.kt
@@ -7,6 +7,8 @@ import com.example.solicitudeshttp.R
 import org.json.JSONObject
 
 class ActivityJSON : AppCompatActivity() {
+    var listaPersonas:ArrayList<Persona>? =null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_j_s_o_n)
@@ -30,14 +32,17 @@ class ActivityJSON : AppCompatActivity() {
         val json = JSONObject(respuesta)
 
         val personas = json.getJSONArray("personas")
+        listaPersonas= ArrayList()
         for (i in 0..personas.length()-1){
             val nombre = personas.getJSONObject(i).getString("nombre")
             val pais = personas.getJSONObject(i).getString("pais")
             val estado = personas.getJSONObject(i).getString("estado")
             val experiencia = personas.getJSONObject(i).getInt("experiencia")
             //mapeix de valors a un objecte
-            val persona = Persona(nombre, pais, estado, experiencia)
-             Log.d("PERSONA", persona.nombre)
+            // val persona = Persona(nombre, pais, estado, experiencia)
+            listaPersonas?.add(Persona(nombre, pais, estado, experiencia))
+             //Log.d("PERSONA", persona.nombre)
         }
+        Log.d("onCreate",listaPersonas?.count().toString())
     }
 }
